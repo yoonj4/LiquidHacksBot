35c380bfb16113bd71b7417d7513b0589dde737c
web-dev <nathankim137311@gmail.com>
2021-11-08 19:57:25 -0800

    changed exp to experience

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE smash_game.fighter_proficiency (
 CREATE TABLE smash_game.`character` (
 	character_id INT auto_increment NOT NULL,
 	prize_money INT DEFAULT 0 NOT NULL,
-	`exp` INT DEFAULT 0 NOT NULL,
+	experience INT DEFAULT 0 NOT NULL,
 	name varchar(20) NOT NULL,
 	stamina INT DEFAULT 100 NOT NULL,
 	is_pro BOOL NOT NULL,
