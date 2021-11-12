0c2b1c422ca6b6e68b8814e267f65d6e6f63e89b
youoy0 <byronkim12@gmail.com>
2021-11-11 21:15:55 -0800

    removed unnecessary awaits and removed connection.end()

--- repository.js
+++ repository.js
@@ -12,7 +12,7 @@ async function insertCharacter(character) {
     const [rows] = await (await connection).execute('insert into smash_game.character(prize_money, experience, name, stamina, is_pro) values(?, ?, ?, ?, ?)', [character.prize_money, character.experience, character.name, character.stamina, character.is_pro]);
     const char_id = rows.insertId;
     const fighter_prof = character.fighter_pool.only();
-    await (await connection).execute('insert into smash_game.`fighter_proficiency`(character_id, experience, name) values(?, ?, ?)', [char_id, fighter_prof.experience, fighter_prof.name]);
+    (await connection).execute('insert into smash_game.`fighter_proficiency`(character_id, experience, name) values(?, ?, ?)', [char_id, fighter_prof.experience, fighter_prof.name]);
 }
 
 async function canLocalStart() {
@@ -24,7 +24,6 @@ async function rest(username) {
     await (await connection).execute('UPDATE smash_game.`character` SET is_resting = true WHERE discord_tag = ?', [username]);
     await new Promise(r => setTimeout(r, 10*1000)); //in milliseconds
     await (await connection).execute('UPDATE smash_game.`character` SET is_resting = false WHERE discord_tag = ?', [username]);
-    await (await connection).end();
 }
 
 module.exports = { 
