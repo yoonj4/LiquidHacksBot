d211d5c09c9eba7d81e4dd5b1a223ab5bc3d1b08
youoy0 <byronkim12@gmail.com>
2021-11-11 21:12:00 -0800

    moved mysql script from /rest to repository

--- commands/rest.js
+++ commands/rest.js
@@ -5,16 +5,10 @@ const db = require('./../repository.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rest')
-		.setDescription('rest for 5 minutes'),
+		.setDescription('rest for 10 seconds'),
 	async execute(interaction) {
-		
 		interaction.reply({ content: 'Resting ... ... ... ', ephemeral: true });
-		
-		await connection.execute('UPDATE smash_game.`character` SET is_resting = true WHERE discord_id = ?', [interaction.user.tag]);
-        await new Promise(r => setTimeout(r, 300*1000)); //in milliseconds
-		await connection.execute('UPDATE smash_game.`character` SET is_resting = false WHERE discord_id = ?', [interaction.user.tag]);
-        interaction.followUp({ content: 'Finished resting. You feel refreshed!', ephemeral: true })
-		
-		connection.end();
+		await db.rest(interaction.user.tag);
+		interaction.followUp({ content: 'Finished resting. You feel refreshed!', ephemeral: true })
 	},
 };
--- repository.js
+++ repository.js
@@ -9,24 +9,26 @@ const connection = mysql.createConnection({
 });
 
 async function insertCharacter(character) {
-    const [rows] = await (await getConnection()).execute('insert into smash_game.character(prize_money, experience, name, stamina, is_pro) values(?, ?, ?, ?, ?)', [character.prize_money, character.experience, character.name, character.stamina, character.is_pro]);
+    const [rows] = await (await connection).execute('insert into smash_game.character(prize_money, experience, name, stamina, is_pro) values(?, ?, ?, ?, ?)', [character.prize_money, character.experience, character.name, character.stamina, character.is_pro]);
     const char_id = rows.insertId;
     const fighter_prof = character.fighter_pool.only();
-    (await connection).execute('insert into smash_game.`fighter_proficiency`(character_id, experience, name) values(?, ?, ?)', [char_id, fighter_prof.experience, fighter_prof.name]);
+    await (await connection).execute('insert into smash_game.`fighter_proficiency`(character_id, experience, name) values(?, ?, ?)', [char_id, fighter_prof.experience, fighter_prof.name]);
 }
 
 async function canLocalStart() {
-    const [rows] = await (await getConnection()).execute('select count(*) as num_players from smash_game.character');
+    const [rows] = await (await connection).execute('select count(*) as num_players from smash_game.character');
     return rows[0].num_players > 1;
 }
 
-async function getConnection() {
-    return await connection;
+async function rest(username) {
+    await (await connection).execute('UPDATE smash_game.`character` SET is_resting = true WHERE discord_tag = ?', [username]);
+    await new Promise(r => setTimeout(r, 10*1000)); //in milliseconds
+    await (await connection).execute('UPDATE smash_game.`character` SET is_resting = false WHERE discord_tag = ?', [username]);
+    await (await connection).end();
 }
 
 module.exports = { 
     insertCharacter,
     canLocalStart,
-    getConnection,
-    connection
+    rest
 };
