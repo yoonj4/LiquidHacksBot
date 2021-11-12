430cc0843a083fa5462a98bb221cbdcd83c355dc
Zach <suks@uw.edu>
2021-11-11 21:20:36 -0800

    Grabbing Nathan's code

---
+++ match.js
@@ -0,0 +1,13 @@
+modules.export = function(player1, player2){
+    // choose fighter
+    console.log("Choose your fighter");
+    let fighter1 = chooseFighter();
+    let fighter2 = chooseFighter();
+
+    // calculate smash level
+    let smashLv1 = calculateSmashLv();
+    let smashLv2 = calculateSmashLv();
+    // calculate each player's odds of winning
+    
+    return;
+}
--- repository.js
+++ repository.js
@@ -9,14 +9,44 @@ const connection = mysql.createConnection({
 });
 
 async function insertCharacter(character) {
-    const [rows] = await (await getConnection()).execute('insert into smash_game.character(prize_money, experience, name, stamina, is_pro) values(?, ?, ?, ?, ?)', [character.prize_money, character.experience, character.name, character.stamina, character.is_pro]);
+    const [rows] = await getConnection().execute('insert into smash_game.character(prize_money, experience, name, stamina, is_pro) values(?, ?, ?, ?, ?)', [character.prize_money, character.experience, character.name, character.stamina, character.is_pro]);
     const char_id = rows.insertId;
     const fighter_prof = character.fighter_pool.only();
-    (await connection).execute('insert into smash_game.`fighter_proficiency`(character_id, experience, name) values(?, ?, ?)', [char_id, fighter_prof.experience, fighter_prof.name]);
+    return connection.execute('insert into smash_game.`fighter_proficiency`(character_id, experience, name) values(?, ?, ?)', [char_id, fighter_prof.experience, fighter_prof.name]);
+}
+
+async function addExperience(exp, character_id) { // Zach - may not need character_id pram?
+    await getConnection().execute('update smash_game.character set experience = experience + exp where character_id'); // Zach - test to make sure that this works
+}
+
+async function addFighterProficiency(fp) {
+    await getConnection().execute('update smash_game.fighter_proficiency set experience = experience + fp where fp_id')
+}
+
+async function levelUp(remainder) {
+    // print message to player
+    // update character experience
+    await getConnection().execute('update smash_game.character set experience = remainder where character_id')
+}
+
+async function decreaseStamina(stamina) {
+    
+}
+
+async function increaseStamina(stamina) {
+
+}
+
+async function getSmashLv() {
+    return;
+}
+
+async function chooseFighter() {
+    return;
 }
 
 async function canLocalStart() {
-    const [rows] = await (await getConnection()).execute('select count(*) as num_players from smash_game.character');
+    const [rows] = await getConnection().execute('select count(*) as num_players from smash_game.character');
     return rows[0].num_players > 1;
 }
 
